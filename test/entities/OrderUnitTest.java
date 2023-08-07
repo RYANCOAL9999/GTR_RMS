021dc0cf66aa79dabb2b99e3c5626413c6c2d600
RYANCOAL9999 <lmf242003@gmail.com>
2023-08-07 17:06:58 +0100

    add TestCase java file

---
+++ test/entities/OrderUnitTest.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entities;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author W22079254
+ */
+public class OrderUnitTest extends TestCase {
+    
+    /**
+     * Test of equal method, of class Vectors.
+     */
+    public void testEqual() {
+        
+    }
+    
+    /**
+     * Test of Entities method, of class Vectors.
+     */
+    public void testEntities(){
+        
+    }
+    
+}
